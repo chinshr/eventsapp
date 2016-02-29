6232ce83e6d578449faf834327fb33a02f1b3845
Tom Johnson <TJohnson@ITs-MacBook-Pro-2.local>
2016-02-29 10:07:25 -0800

    don't create an agenda item when the events show page is created.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,8 +10,6 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-    #TODO: This blows.  Why create a new agenda_item in the events controller when I show the event?
-    @agenda_item = AgendaItem.new
   end
 
   # GET /events/new
