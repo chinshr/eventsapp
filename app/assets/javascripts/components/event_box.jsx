ea8acd3669337819cd2a62220a51108d318b9090
Tom Johnson <TJohnson@ITs-MacBook-Pro-2.local>
2016-04-14 17:40:37 -0700

    moved components to separate files

---
+++ app/assets/javascripts/components/event_box.jsx
@@ -0,0 +1,18 @@
+class EventBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: this.props.data
+    }
+  }
+  render() {
+    console.log('this.props...', this.props);
+    return (
+      <div className="eventBox">
+        <h1>Events</h1>
+        <EventList data={this.state.data} />
+      </div>
+    );
+  }
+};
